2b745c6df8516af400e14bd79964dbc2148d72ab
onecelldm <pokesm.pe@gmail.com>
2022-07-01 21:29:52 +0300

    Project reorganization started

---
+++ VKAvaloniaPlayer/ViewModels/Audios/MusicFromAlbumViewModel.cs
@@ -0,0 +1,79 @@
+﻿using Avalonia.Layout;
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using VKAvaloniaPlayer.ETC;
+using VKAvaloniaPlayer.Models;
+using VKAvaloniaPlayer.ViewModels.Base;
+
+using VkNet.Model.RequestParams;
+
+namespace VKAvaloniaPlayer.ViewModels.Audios
+{
+    public sealed class MusicFromAlbumViewModel : VkDataViewModelBase
+    {
+        public delegate void AudioRemove(AudioModel audioModel);
+
+        public MusicFromAlbumViewModel(AudioAlbumModel audioAlbumModel)
+        {
+            Album = audioAlbumModel;
+
+            StartSearchObservable(new TimeSpan(0, 0, 0, 0, 500));
+            StartScrollChangedObservable(LoadMusicsAction, Orientation.Vertical);
+
+            AudioRemoveEvent += MusicFromAlbumViewModel_AudioRemoveEvent;
+
+            if (Album.OwnerID == GlobalVars.VkApi.UserId && !Album.IsFollowing)
+                AudioListButtons.AudioAddIsVisible = false;
+            else
+                AudioListButtons.AudioRemoveIsVisible = false;
+            AudioListButtons.Album = Album;
+        }
+
+        private AudioAlbumModel Album { get; }
+
+        public static event AudioRemove AudioRemoveEvent;
+
+        public static void AudioRemoveEventCall(AudioModel audioModel)
+        {
+            AudioRemoveEvent?.Invoke(audioModel);
+        }
+
+        private void MusicFromAlbumViewModel_AudioRemoveEvent(AudioModel audioModel)
+        {
+            _AllDataCollection.Remove(audioModel);
+        }
+
+        public override void LoadData()
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    var res = GlobalVars.VkApi?.Audio.Get(new AudioGetParams
+                    {
+                        Count = 500,
+                        Offset = (uint)Offset,
+                        PlaylistId = Album.ID
+                    });
+                    if (res != null)
+                    {
+                        DataCollection.AddRange(res);
+                        ResponseCount = res.Count;
+
+                        Task.Run(() => { DataCollection.StartLoadImages(); });
+                        Offset += res.Count;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                }
+
+                IsLoading = false;
+            });
+        }
+    }
+}
---
+++ VKAvaloniaPlayer/ViewModels/Audios/RecomendationsViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Avalonia.Layout;
+
+using System;
+using System.Threading.Tasks;
+
+using VKAvaloniaPlayer.ETC;
+using VKAvaloniaPlayer.ViewModels.Base;
+
+using VkNet.Exception;
+
+namespace VKAvaloniaPlayer.ViewModels.Audios
+{
+    public sealed class RecomendationsViewModel : VkDataViewModelBase
+    {
+        public RecomendationsViewModel()
+        {
+            StartSearchObservable(new TimeSpan(0, 0, 0, 0, 500));
+            StartScrollChangedObservable(LoadMusicsAction, Orientation.Vertical);
+            AudioListButtons.AudioRemoveIsVisible = false;
+        }
+
+
+        public override void LoadData()
+        {
+            Task.Run(() =>
+            {
+                IsLoading = true;
+                try
+                {
+                    var res = GlobalVars.VkApi?.Audio.GetRecommendations(count: 500, offset: (uint)Offset);
+                    if (res != null)
+                    {
+                        DataCollection.AddRange(res);
+
+                        Task.Run(() => { DataCollection.StartLoadImages(); });
+                        Offset += res.Count;
+                        ResponseCount = res.Count;
+                    }
+                }
+                catch (VkAuthorizationException exception)
+                {
+                }
+
+                IsLoading = false;
+            });
+        }
+    }
+}
